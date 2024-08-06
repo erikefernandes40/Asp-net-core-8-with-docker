3afd11a4979447ab0827c9fdf371b08635ee90d4
Erick Fernando <erikefernandes40@gmail.com>
2024-08-06 16:22:39 -0300

    feat: adiciona repositorio EmployeeRepository

---
+++ FirstApi/Infrastructure/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using FirstApi.Model;
+
+namespace FirstApi.Infrastructure
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+
+        private readonly ConnectionContext _context = new ConnectionContext();
+        public void Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+
+        public List<Employee> Get()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+}
