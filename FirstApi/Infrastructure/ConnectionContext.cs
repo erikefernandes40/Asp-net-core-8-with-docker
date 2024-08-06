c8e37f52b67f32c542ec04928873e15c0fdb8817
Erick Fernando <erikefernandes40@gmail.com>
2024-08-06 16:19:10 -0300

    feat: adiciona configuração de conexão com banco de dados

---
+++ FirstApi/Infrastructure/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using FirstApi.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FirstApi.Infrastructure
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Host=postgres;Port=5432;Database=employee_sample;Username=postgres;Password=postgres";
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
