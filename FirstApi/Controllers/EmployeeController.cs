a555ee540e775a63672a57ddadaa44f960a4a8ac
Erick Fernando <erikefernandes40@gmail.com>
2024-08-06 16:23:16 -0300

    feat: adiciona rotas para criar e recuperar dados referentes aos employees

---
+++ FirstApi/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using FirstApi.Model;
+using FirstApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [ApiController]
+    [Route("api/vi/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+        }
+
+        [HttpPost]
+
+        public IActionResult Add(EmployeeViewModel employeeView)
+        {
+            var employee = new Employee(employeeView.Name, employeeView.Age, null);
+
+            _employeeRepository.Add(employee);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var emplyess = _employeeRepository.Get();
+            return Ok(emplyess);
+        }
+    }
+}
