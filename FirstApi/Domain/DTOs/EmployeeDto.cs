0377d1d8507cb94e4ebb6917faeffccd9300facc
Erick Fernando <erikefernandes40@gmail.com>
2024-08-08 15:11:48 -0300

    feat: adiciona dto Employee

---
+++ FirstApi/Domain/DTOs/EmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstApi.Domain.DTOs
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Photo { get; set; }
+    }
+}
