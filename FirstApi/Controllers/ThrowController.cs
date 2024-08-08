6534a9204611077c854a78c1f1bbd3f3cc59eae7
Erick Fernando <erikefernandes40@gmail.com>
2024-08-08 14:37:07 -0300

    feat: adiciona controller para tratar exceptions

---
+++ FirstApi/Controllers/ThrowController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ThrowController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            return Problem();
+        }
+
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment(
+            [FromServices] IHostEnvironment hostEnviroment)
+        {
+            if(!hostEnviroment.IsDevelopment())
+            {
+                return NotFound();
+            }
+
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                detail: exceptionHandlerFeature.Error.StackTrace,
+                title: exceptionHandlerFeature.Error.Message
+                );
+        }
+    }
+}
--- FirstApi/Program.cs
+++ FirstApi/Program.cs
@@ -42,8 +42,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/error-development");
     app.UseSwagger();
     app.UseSwaggerUI();
+} else
+{
+    app.UseExceptionHandler("/error");
 }
 
 //app.UseHttpsRedirection();
