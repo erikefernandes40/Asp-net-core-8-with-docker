93e296634f8775fdfa0251d26041ba98b53b1951
Erick Fernando <erikefernandes40@gmail.com>
2024-08-08 17:40:32 -0300

    feat: adiciona classe Company

---
+++ FirstApi/Domain/Model/CompanyAggregate/Company.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstApi.Domain.Model.CompanyAggregate
+{
+    [Table("company")]
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Company(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
