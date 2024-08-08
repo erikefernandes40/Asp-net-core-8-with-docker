969fb8669918cf6d2421472b28053494a98583ac
Erick Fernando <erikefernandes40@gmail.com>
2024-08-08 16:07:24 -0300

    feat: adiciona automapper DomainToDtoMapping

---
+++ FirstApi/Application/Mapping/DomainToDtoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FirstApi.Domain.DTOs;
+using FirstApi.Domain.Model;
+
+namespace FirstApi.Application.Mapping
+{
+    public class DomainToDtoMapping : Profile
+    {
+        public DomainToDtoMapping() 
+        {
+            CreateMap<Employee, EmployeeDto>();
+        }
+    }
+}
