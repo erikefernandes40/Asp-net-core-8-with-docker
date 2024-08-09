cfb3ea6f0ca1cdc1a7004a88eb552d8f475d0168
Erick Fernando <erikefernandes40@gmail.com>
2024-08-09 11:43:49 -0300

    feat: adiciona controller no versionamento V2 para fim de testes

---
+++ FirstApi/Controllers/V2/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers.V2
+{
+    [ApiVersion(2.0)]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("employee v2");
+        }
+    }
+}
